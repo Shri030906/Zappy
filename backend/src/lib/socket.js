ee1ee08527f7eb9d26517f03a3e4e0c8af4fb3a6
SHRIJIV <shrijivtambadkar112@gmail.com>
2025-04-19 05:35:47 +0530

    Pro

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import User from "../models/user.model.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -12,24 +13,43 @@ const io = new Server(server, {
 });
 
 export function getReceiverSocketId(userId) {
-  return userSocketMap[userId];
+  const sockets = userSocketMap[userId];
+  if (!sockets || sockets.length === 0) return null;
+  // Return the first socket id for the user
+  return sockets[0];
 }
 
 // used to store online users
-const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {}; // {userId: [socketId1, socketId2, ...]}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  if (userId) {
+    if (!userSocketMap[userId]) {
+      userSocketMap[userId] = [];
+    }
+    userSocketMap[userId].push(socket.id);
+  }
 
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", async () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    if (userId && userSocketMap[userId]) {
+      userSocketMap[userId] = userSocketMap[userId].filter((id) => id !== socket.id);
+      if (userSocketMap[userId].length === 0) {
+        delete userSocketMap[userId];
+        // Update lastOnline when user fully disconnects
+        try {
+          await User.findByIdAndUpdate(userId, { lastOnline: new Date() });
+        } catch (error) {
+          console.error("Error updating lastOnline:", error);
+        }
+      }
+    }
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
